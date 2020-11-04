f2962e54c27dc627b2d39cc30b1a571ffa126fde
ccjameslai <spidyjames@gmail.com>
2020-11-05 00:18:56 +0800

    handle event and enumerate

--- ScrollingText/CreateWindow/main.cpp
+++ ScrollingText/CreateWindow/main.cpp
@@ -1,11 +1,21 @@
 #include <sfml/Window.hpp>
+#include <iostream>
 
 int main() {
-	// sf::VideoMode mode(400, 200);
-	// sf::String title("Hello");
 	sf::Window w(sf::VideoMode(400, 200), "Hello");
 
-	while (true) {
+	while (w.isOpen()) {
+		sf::Event evt;
+		if (w.pollEvent(evt)) {
+			if (evt.type == sf::Event::Closed) {
+				std::cout << "close" << std::endl;
+				w.close();
+			} else if (evt.type == sf::Event::Resized) {
+				std::cout << "Resize" << std::endl;
+			}
 
+		}
 	}
+
+	return 0;
 }
