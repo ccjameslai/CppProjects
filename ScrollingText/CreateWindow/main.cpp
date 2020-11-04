b6e78545b420d673797f044db1c67389bdcc14ea
ccjameslai <spidyjames@gmail.com>
2020-11-04 23:49:40 +0800

    anonymous object and implicitly type conversion

--- ScrollingText/CreateWindow/main.cpp
+++ ScrollingText/CreateWindow/main.cpp
@@ -1,9 +1,9 @@
 #include <sfml/Window.hpp>
 
 int main() {
-	sf::VideoMode mode(400, 200);
-	sf::String title("Hello");
-	sf::Window w(mode, title);
+	// sf::VideoMode mode(400, 200);
+	// sf::String title("Hello");
+	sf::Window w(sf::VideoMode(400, 200), "Hello");
 
 	while (true) {
 
