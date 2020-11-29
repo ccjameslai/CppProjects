50402e01fbc7366daf486c1b22a9b1d81f81cb83
ccjameslai <spidyjames@gmail.com>
2020-11-29 16:58:55 +0800

    Post a texure

---
+++ SnakeGame/SnakeGame/main.cpp
@@ -0,0 +1,18 @@
+#include <sfml/Graphics.hpp>
+
+int main() {
+	sf::VideoMode mode(400, 300);
+	sf::RenderWindow w(mode, L"³g¦Y³D");
+
+	while (w.isOpen()) {
+		sf::Event evt;
+		if (w.pollEvent(evt)) {
+			if (evt.type == sf::Event::Closed) {
+				w.close();
+			}
+		}
+
+		w.display();
+	}
+	return EXIT_SUCCESS
+}
