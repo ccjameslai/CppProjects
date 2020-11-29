798e32d783ffe50537d7db2a25b8a59553fe3a5f
ccjameslai <spidyjames@gmail.com>
2020-11-29 22:54:41 +0800

    Set the exact position

--- SnakeGame/SnakeGame/main.cpp
+++ SnakeGame/SnakeGame/main.cpp
@@ -1,11 +1,22 @@
 #include <sfml/Graphics.hpp>
+#include <iostream>
 
 int main() {
-	sf::VideoMode mode(400, 300);
-	sf::RenderWindow w(mode, L"³g¦Y³D");
 	sf::Texture texture;
-	texture.loadFromFile("block.png");
-	sf::Sprite image(texture);
+	if (!texture.loadFromFile("block.png")) {
+		std::cout << "Texture not found!!" << std::endl;
+	}
+	
+	sf::Sprite block(texture);
+	sf::Vector2<float> blockSize(block.getGlobalBounds().width, block.getGlobalBounds().height);
+
+	sf::Vector2<int> fieldSize(20, 15);
+	sf::Vector2f head(3, 4);
+
+	sf::VideoMode mode(blockSize.x * fieldSize.x, blockSize.y * fieldSize.y);
+	sf::RenderWindow w(mode, L"³g¦Y³D");
+	
+	
 
 	while (w.isOpen()) {
 		sf::Event evt;
@@ -15,7 +26,17 @@ int main() {
 			}
 		}
 
-		w.draw(image);
+		block.setPosition(head.x * fieldSize.x, head.y * fieldSize.y);
+		w.draw(block);
+
+		/*for (int i = 0; i < fieldSize.x; i++) {
+			for (int j = 0; j < fieldSize.y; j++) {
+				sf::Vector2<float> pos(i * blockSize.x, j * blockSize.y);
+				block.setPosition(pos);
+				w.draw(block);
+			}
+		}*/
+
 		w.display();
 		
 	}
