5481271da13fff4664703c0a9d1ad14960d2f764
ccjameslai <spidyjames@gmail.com>
2020-12-27 00:45:31 +0800

    apply nlohmann::json to parse data

--- COVID-19/main.cpp
+++ COVID-19/main.cpp
@@ -1,19 +1,16 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <nlohmann/json.hpp>
 
 using namespace std;
 
 int main() {
-
 	const string data = "{\"TotalConfirmed\": 79828488, \"TotalDeaths\" : 1750474, \"TotalRecovered\" : 44992928}";
 
-	int confirmedPos = data.find(":") + 1;
-	int confirmedEnd = data.find(",");
-	string confirmed = data.substr(confirmedPos, confirmedEnd - confirmedPos);
+	nlohmann::json j = nlohmann::json::parse(data);
 
-	int deadthPos = data.find(":", confirmedEnd) + 1;
-	int deadthEnd = data.find(",", deadthPos);
-	string deadth = data.substr(deadthPos, deadthEnd - deadthPos);
+	int confirmed = j["TotalConfirmed"];
+	int death = j["TotalDeaths"];
 
 	sf::RenderWindow w(sf::VideoMode(400, 160), L"COVID-19");
 
@@ -30,21 +27,21 @@ int main() {
 	sf::Sprite confirmedSprite(confirmedTexture);
 	confirmedSprite.setPosition(15, 20);
 
-	sf::Sprite deadthSprite(deathTexture);
-	deadthSprite.setPosition(15, 80);
+	sf::Sprite deathSprite(deathTexture);
+	deathSprite.setPosition(15, 80);
 
 	sf::Font font;
 	if (!font.loadFromFile("consola.ttf")) {
 		return EXIT_FAILURE;
 	}
 
-	sf::String confirmedString(confirmed);
+	sf::String confirmedString(std::to_string(confirmed));
 	sf::Text confirmedText(confirmedString, font);
 	confirmedText.setPosition(200, 20);
 
-	sf::String deadthString(deadth);
-	sf::Text deadthText(deadthString, font);
-	deadthText.setPosition(200, 80);
+	sf::String deathString(std::to_string(death));
+	sf::Text deathText(deathString, font);
+	deathText.setPosition(200, 80);
 
 	while (w.isOpen()) {
 		sf::Event evt;
@@ -56,9 +53,9 @@ int main() {
 
 		w.clear();
 		w.draw(confirmedSprite);
-		w.draw(deadthSprite);
+		w.draw(deathSprite);
 		w.draw(confirmedText);
-		w.draw(deadthText);
+		w.draw(deathText);
 		w.display();
 	}
 
