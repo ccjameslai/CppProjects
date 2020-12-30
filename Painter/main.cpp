26924f577a5ef6384b529f82691710ee54908c9d
ccjameslai <spidyjames@gmail.com>
2020-12-30 21:33:52 +0800

    add seven different colors of pens

--- Painter/main.cpp
+++ Painter/main.cpp
@@ -1,6 +1,8 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
+using namespace std;
+
 int main() {
 	sf::RenderWindow w(sf::VideoMode(642, 640), L"¤pµe®a");
 
@@ -8,8 +10,29 @@ int main() {
 	cursor.setFillColor(sf::Color::Black);
 	cursor.setOrigin(10, 10);
 
-	sf::RectangleShape rect(sf::Vector2f(10,10));
-	rect.setFillColor(sf::Color::Black);
+	
+	vector<sf::Color> colors = {
+		sf::Color::Black,
+		sf::Color::Red,
+		sf::Color(175,105,86),
+		sf::Color::Blue,
+		sf::Color::Yellow,
+		sf::Color::Cyan,
+		sf::Color::Magenta,
+	};
+
+	float swatchOffset = 20;
+	vector<sf::RectangleShape> swatches;
+	for (auto color : colors) {
+		sf::RectangleShape swatch(sf::Vector2f(20,20));
+		swatch.setFillColor(color);
+		swatch.setOutlineColor(sf::Color(230,230,230));
+		swatch.setOutlineThickness(2);
+		swatch.setPosition(swatchOffset, 10);
+		swatches.push_back(swatch);
+
+		swatchOffset += 20;
+	}
 
 	sf::Texture backgroundTexture;
 	if (!backgroundTexture.loadFromFile("background.png")) {
@@ -33,18 +56,35 @@ int main() {
 			if (evt.type == sf::Event::Closed) {
 				w.close();
 			}
+
+			if (evt.type == sf::Event::MouseButtonPressed) {
+				if (evt.mouseButton.button == sf::Mouse::Left) {
+					for (const auto& s : swatches) {
+						if (s.getGlobalBounds().
+							contains(sf::Vector2f(evt.mouseButton.x, evt.mouseButton.y))){
+							cursor.setFillColor(s.getFillColor());
+						}
+					}
+				}
+			}
 		}
 
 		sf::Vector2i mousePos = sf::Mouse::getPosition(w);
 
-		cursor.setPosition(sf::Vector2f(mousePos) - canvasOffset);
-		canvas.draw(cursor);
-		canvas.display();
+		if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+			cursor.setPosition(sf::Vector2f(mousePos) - canvasOffset);
+			canvas.draw(cursor);
+			canvas.display();
+		}
 
 		w.clear(sf::Color::White);
 		w.draw(backgroundSprite);
 		w.draw(canvasSprite);
-		w.draw(rect);
+
+		for (const auto& s : swatches) {
+			w.draw(s);
+		}
+
 		cursor.setPosition(sf::Vector2f(mousePos));
 		w.draw(cursor);
 		w.display();
