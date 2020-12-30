17a34ed8b04c30f594076dfd93ddffb24ce360b1
ccjameslai <spidyjames@gmail.com>
2020-12-30 20:37:08 +0800

    save the trajectory of cursor on the window

--- Painter/main.cpp
+++ Painter/main.cpp
@@ -6,7 +6,11 @@ int main() {
 
 	sf::CircleShape cursor(10);
 	cursor.setFillColor(sf::Color::Black);
-	
+	cursor.setOrigin(10, 10);
+
+	sf::RectangleShape rect(sf::Vector2f(10,10));
+	rect.setFillColor(sf::Color::Black);
+
 	sf::Texture backgroundTexture;
 	if (!backgroundTexture.loadFromFile("background.png")) {
 		return EXIT_FAILURE;
@@ -15,7 +19,14 @@ int main() {
 	sf::Sprite backgroundSprite(backgroundTexture);
 	backgroundSprite.setPosition(0, 40);
 
-	sf::Vector2f shift(10, 10);
+	sf::RenderTexture canvas;
+	canvas.create(600, 560);
+	canvas.clear(sf::Color::White);
+
+	sf::Vector2f canvasOffset(20, 60);
+	sf::Sprite canvasSprite(canvas.getTexture());
+	canvasSprite.setPosition(canvasOffset);
+
 	while (w.isOpen()) {
 		sf::Event evt;
 		if (w.pollEvent(evt)) {
@@ -25,11 +36,16 @@ int main() {
 		}
 
 		sf::Vector2i mousePos = sf::Mouse::getPosition(w);
-		cursor.setPosition(sf::Vector2f(mousePos) - shift);
-		
-		
+
+		cursor.setPosition(sf::Vector2f(mousePos) - canvasOffset);
+		canvas.draw(cursor);
+		canvas.display();
+
 		w.clear(sf::Color::White);
 		w.draw(backgroundSprite);
+		w.draw(canvasSprite);
+		w.draw(rect);
+		cursor.setPosition(sf::Vector2f(mousePos));
 		w.draw(cursor);
 		w.display();
 	}
